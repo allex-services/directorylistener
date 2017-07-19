40567726daa14b776f1151ea360d3d859a95995a
veljkopopovic <v.s.popovic@gmail.com>
2017-07-19 09:39:32 +0200

    Updating dependecies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createServicePack(execlib) {
   'use strict';
   return {
     service: {
-      dependencies: ['allex_needingservice', 'allex:directory:lib', 'allex:timer:lib']
+      dependencies: ['allex_needingservice', 'allex_directorylib', 'allex_timerlib']
     },
     sinkmap: {
       dependencies: ['allex_needingservice']
